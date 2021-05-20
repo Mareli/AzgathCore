5b8652cda3fab02487c6ef39584ada2fb5c64e7c
Nehyren <64296128+AzgathCore@users.noreply.github.com>
2021-05-20 05:07:33 +0200

    Delete sql directory

--- sql/Azgath/hotfixes/2020_08_08_00_fix_alliance_cloak_of_coordination.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `az_hotfixes`.`hotfix_data`(`Id`, `TableHash`, `RecordId`, `Deleted`, `VerifiedBuild`) VALUES (56645, 1073915313, 12926, 0, 0);
-INSERT INTO `az_hotfixes`.`item_effect`(`ID`, `LegacySlotIndex`, `TriggerType`, `Charges`, `CoolDownMSec`, `CategoryCoolDownMSec`, `SpellCategoryID`, `SpellID`, `ChrSpecializationID`, `ParentItemID`, `VerifiedBuild`) VALUES (12926, 0, 0, 0, 7200000, -1, 0, 3561, 0, 65360, 0);
--- sql/Azgath/world/NPCTeleport/teleporter_remove.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS `npc_teleport`;
-DELETE FROM creature_template WHERE entry = 1001000;
