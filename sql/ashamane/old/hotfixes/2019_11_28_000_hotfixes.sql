4912a46b151e3743379a74fb66e399b6cefae528
Nehyren <64296128+AzgathCore@users.noreply.github.com>
2021-05-20 04:39:50 +0200

    Delete sql/ashamane/old/hotfixes directory

--- sql/ashamane/old/hotfixes/2019_11_28_000_hotfixes.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `azerite_empowered_item`;
-DROP TABLE IF EXISTS `azerite_item`;
-DROP TABLE IF EXISTS `azerite_item_milestone_power`;
-DROP TABLE IF EXISTS `azerite_power`;
-DROP TABLE IF EXISTS `azerite_power_set_member`;
-DROP TABLE IF EXISTS `azerite_tier_unlock`;
--- sql/ashamane/old/hotfixes/2020_02_19_00_hotfixes.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `spec_set_member` MODIFY COLUMN `SpecSetID` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `ChrSpecializationID`;
