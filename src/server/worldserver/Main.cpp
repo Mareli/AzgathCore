4f273dc6c22ee8509ba7e00683b523c31204d2b9
Nehyren <64296128+AzgathCore@users.noreply.github.com>
2021-05-22 07:20:40 +0200

    Added Eluna Lua Engine

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -49,6 +49,9 @@
 #include "World.h"
 #include "WorldSocket.h"
 #include "WorldSocketMgr.h"
+#ifdef ELUNA
+#include "LuaEngine.h"
+#endif
 #include <openssl/opensslv.h>
 #include <openssl/crypto.h>
 #include <boost/asio/signal_set.hpp>
@@ -264,6 +267,9 @@ extern int main(int argc, char** argv)
         sInstanceSaveMgr->Unload();
         sOutdoorPvPMgr->Die();                    // unload it before MapManager
         sMapMgr->UnloadAll();                     // unload all grids (including locked in memory)
+#ifdef ELUNA
+        Eluna::Uninitialize();
+#endif
     });
 
     // Start the Remote Access port (acceptor) if enabled
